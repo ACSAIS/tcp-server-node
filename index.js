b671587b42e22eeb90786cae0e0d9c571c851d02
ACSAIS <kliukovkin@gmail.com>
2022-01-20 18:38:52 +0300

    server socket

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const net = require('net'); 
 const HOST = '0.0.0.0'; 
 const PORT = 3000; 
 
-const server = new net.Server(); 
+const server = new net.Server((socket) => {
+    socket.on("data", (data) => {
+        console.log(`received data ${data}`);
+        console.log(`from host: ${socket.remoteAddress}, port: ${socket.remotePort}`);
+    })
+}); 
 
 server.listen(PORT, HOST);
 
